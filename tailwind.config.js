bddc90980a5f99947299880b1d7cc400c899c833
Kubra Kuscu <kuscukubra58@gmail.com>
2022-09-03 23:59:41 +0300

    deneme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        graybg: "#F5F5F7",
+        acikmavi : "#4FC6F8"
+      }
+    },
+  },
+  plugins: [],
+}
